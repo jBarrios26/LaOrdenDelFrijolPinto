ac81565f436bc5f038adc04c261ca20100dd4c3c
GizehG <gizeh.garcia@galileo.edu>
2021-03-13 21:31:35 -0600

    Moved file fixed-point.h haha

---
+++ threads/fixed-point.h
@@ -0,0 +1,21 @@
+/* Fixed-Point Real Arithmetic */
+#ifndef THREADS_FIXED_POINT_H
+#define THREADS_FIXED_POINT_H
+
+
+/* 17 bits before decimal point, 14 bits after it.
+   1 bit for the sign. */
+#define P 17
+#define Q 14
+#define F 1 << (Q)
+
+#define CONVERT_TO_FIXED_POINT(n)  (n)*(F)      //n is integer
+#define CONVERT_TO_INTZERO(x) (x)/(F)           //x is fixed point and signed. This one rounds to 0
+
+#define ADD(x,y) (x)+(y)                        //x and y are fixed-point numbers
+#define SUBS(x,y) (x)-(y) 
+#define MULTI(x,y) (x)*(y) 
+#define DIV(x,y) (x)/(y)
+
+
+#endif
