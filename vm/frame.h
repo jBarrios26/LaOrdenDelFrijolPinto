4db10a5af838d1fc242105ed49ae398ca523c5b4
jBarrios26 <jorge.barrios@galileo.edu>
2021-04-18 20:59:09 -0700

    frame entry base

---
+++ vm/frame.h
@@ -0,0 +1,36 @@
+#ifndef VM_FRAME_H
+#define VM_FRAME_H
+
+#include <debug.h>
+#include <list.h>
+#include <hash.h>
+#include "threads/synch.h"
+#include <stdint.h>
+
+struct list frame_table; 
+struct lock frame_lock; 
+
+/*
+    The frame table contains the frame entries that all of the processes 
+    are holding. Save the frame pointer, which is a kernel page, and saves the user page, the page that 
+    the user sees. 
+
+*/
+struct frame_entry
+{
+    uint32_t* frame;
+    void* upage;
+
+    struct thread* owner; 
+
+    // Save if it is data, file or executable. 
+    // Save if it is pinned.
+}
+
+void frame_init(void);
+
+uint32_t* frame_allocate(void); 
+void frame_deallocate(uint32_t* page); 
+
+
+#endif
